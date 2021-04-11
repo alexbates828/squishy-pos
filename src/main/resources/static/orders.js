281b509be58c961959b4219db9ca4adcefbb6a20
Alex Bates <alexbates828@gmail.com>
2021-04-11 12:47:55 -0500

    Doctor up terminal buttons and add preliminary subtraction and finishing logic

--- src/main/resources/static/orders.js
+++ src/main/resources/static/orders.js
@@ -11,8 +11,16 @@ function getPriceDisplay() {
 
 function handleAdd() {
     let price = getPriceDisplay();
-    console.log("Price displayed:" + price)
+    console.log("Price displayed:" + price);
     addPrice(price);
+    updatePricesDisplay();
+    resetPriceDisplay();
+}
+
+function handleSubtract() {
+    let price = getPriceDisplay();
+    console.log("Price displayed:" + price);
+    removePrice(price);
     updatePricesDisplay();
     resetPriceDisplay();
 }
@@ -30,5 +38,26 @@ function removePrice(price) {
 }
 
 function updatePricesDisplay() {
-    return;
+    console.log("Prices: " + prices);
+}
+
+function resetPricesDisplay() {
+    prices = [];
+    updatePricesDisplay();
+}
+
+function showTotal(total) {
+    console.log(total);
+}
+
+function handleFinish() {
+    // TODO: make this an API call
+    let total = sumArray(prices);
+    resetPricesDisplay();
+    resetPriceDisplay();
+    showTotal(total)
+}
+
+function sumArray(arr) {
+    return arr.reduce((a, b) => a + parseFloat(b), 0);
 }
